d59c38f5b8673d7bd4c244ee5010294230792cbb
Ravil Galeyev <dehasi@yandex.ru>
2020-07-28 22:47:24 +0200

    Add jpa

---
+++ src/main/java/jpa/PersistentCustomerBase.kt
@@ -0,0 +1,25 @@
+package jpa
+
+import persistance.model.Customer
+import persistance.model.CustomerBase
+import java.time.LocalDateTime
+import javax.persistence.EntityManager
+import javax.persistence.Query
+
+class PersistentCustomerBase(private val entityManager: EntityManager) : CustomerBase {
+
+    override fun addCustomer(customer: Customer) {
+        entityManager.persist(customer)
+    }
+
+    override fun customersWithExpiredCreditCardsAsOf(deadline: LocalDateTime): List<Customer> {
+        val query: Query = entityManager.createQuery("""
+            SELECT c from Customer c, CreditCardDetails d 
+            WHERE d member of c.paymentMethods  
+            AND d.expiryDate < :deadline""".trimIndent())
+        query.setParameter("deadline", deadline)
+
+        @Suppress("UNCHECKED_CAST")
+        return query.resultList as List<Customer>
+    }
+}
