8ac27859956a1d0e4ea646bc90edbf029b6dd49d
Ravil Galeyev <dehasi@yandex.ru>
2020-07-28 22:35:18 +0200

    Add model

---
+++ src/main/java/persistance/model/CreditCardDetails.kt
@@ -0,0 +1,17 @@
+package persistance.model
+
+import java.time.LocalDateTime
+import javax.persistence.CascadeType.PERSIST
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.ManyToOne
+
+@Entity
+data class CreditCardDetails(
+    val cardNumber: String,
+    val nameOnCard: String,
+    val expiryDate: LocalDateTime,
+
+    @ManyToOne(cascade = [PERSIST], fetch = FetchType.EAGER, optional = false)
+    val billingAddress: Address
+) : PaymentMethod(42)
