93a0e87b4f1bd11cda200c118c44a902c47acd40
Ravil Galeyev <dehasi@yandex.ru>
2020-08-10 01:21:33 +0200

    Add siomp test

---
+++ src/test/java/persistance/PersisteanceTest.kt
@@ -0,0 +1,22 @@
+package persistance
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import persistance.model.Address
+import javax.persistence.Persistence
+
+
+class PersisteanceTest {
+    var em = Persistence.createEntityManagerFactory("TestUnit")
+        .createEntityManager()
+
+    @Test fun simple_test() {
+        val  address = Address(23,"","","","")
+
+        em.persist(address);
+
+        val fronDB = em.find(Address::class.java, 23)
+
+        assertThat(fronDB).isEqualTo(address)
+    }
+}
