2423d0d8036e3927d1081937d8f0723f06250139
Ravil Galeyev <dehasi@yandex.ru>
2020-08-10 22:06:57 +0200

    Finish builders

--- src/test/java/persistence/PersistentCustomerBaseTest.kt
+++ src/test/java/persistence/PersistentCustomerBaseTest.kt
@@ -1,22 +1,42 @@
 package persistence
 
 import jpa.PersistentCustomerBase
+import org.junit.After
+import org.junit.Before
 import org.junit.jupiter.api.Test
 import persistence.builders.CustomerBuilder
 import javax.persistence.Persistence
 
+
 class PersistentCustomerBaseTest {
 
-    private var entityManager = Persistence.createEntityManagerFactory("GOOS")
-        .createEntityManager()
+    private val factory = Persistence.createEntityManagerFactory("GOOS")
+    private var entityManager = factory.createEntityManager()
+    private val transactor = JPATransactor(entityManager)
+
     private val customerBase = PersistentCustomerBase(entityManager)
+
+    @Before fun cleanDatabase() {
+        DatabaseCleaner(entityManager).clean()
+    }
 
     @Test fun `find customers with credit cards that are about to expire`() {
         val deadLine = "6 Jun 2009"
 
     }
 
-    private fun addCustomers(customers : Array<CustomerBuilder>){
+    private fun addCustomers(customers: Array<CustomerBuilder>) {
+        transactor.perform(object : UnitOfWork {
+            override fun work() {
+                customers.forEach {
+                    customerBase.addCustomer(it.build())
+                }
+            }
+        })
+    }
 
+    @After fun tearDown() {
+        entityManager.close()
+        factory.close()
     }
 }
--- src/test/java/persistence/builders/CustomerBuilder.kt
+++ src/test/java/persistence/builders/CustomerBuilder.kt
@@ -1,4 +1,69 @@
 package persistence.builders
 
-class CustomerBuilder {
+import persistence.model.Address
+import persistence.model.AuctionSiteCredentials
+import persistence.model.Customer
+import persistence.model.PaymentMethod
+import java.util.Collections.emptySet
+
+
+class CustomerBuilder : AbstractBuilder<CustomerBuilder, Customer>() {
+
+    private var name = "customer"
+    private var email = "customer@example.com"
+    private var addressBuilder: Builder<Address> = AddressBuilder()
+    private var paymentMethods: Set<Builder<out PaymentMethod>> = emptySet()
+    private var auctionSitesUsed: Set<Builder<out AuctionSiteCredentials>> = emptySet()
+
+    fun withName(name: String): CustomerBuilder? {
+        val other = clone()
+        other.name = name
+        return other
+    }
+
+    fun withEmailAddress(email: String): CustomerBuilder? {
+        val other = clone()
+        other.email = email
+        return other
+    }
+
+    fun withAddress(addressBuilder: Builder<Address>): CustomerBuilder? {
+        val other = clone()
+        other.addressBuilder = addressBuilder
+        return other
+    }
+
+    fun withPaymentMethods(vararg paymentMethodBuilders: Builder<out PaymentMethod>): CustomerBuilder? {
+        val other = clone()
+        other.paymentMethods = setOf<PaymentMethod>(*paymentMethodBuilders)
+        return other
+    }
+
+    fun usingAuctionSites(vararg auctionSiteLoginBuilders: Builder<AuctionSiteCredentials>): CustomerBuilder? {
+        val other = clone()
+        other.auctionSitesUsed = setOf<AuctionSiteCredentials>(*auctionSiteLoginBuilders)
+        return other
+    }
+
+    private fun <T> setOf(vararg builders: Builder<out T>): Set<Builder<out T>> {
+        return mutableSetOf(*builders)
+    }
+
+    override fun build(): Customer {
+        return Customer(0, name, email, addressBuilder.build(), buildSet(paymentMethods), buildSet(auctionSitesUsed))
+    }
+
+    companion object {
+        fun aCustomer(): CustomerBuilder {
+            return CustomerBuilder()
+        }
+
+        private fun <T> buildSet(builders: Set<Builder<out T>>): Set<T> {
+            val set = HashSet<T>()
+            for (builder in builders) {
+                set.add(builder.build())
+            }
+            return set
+        }
+    }
 }
