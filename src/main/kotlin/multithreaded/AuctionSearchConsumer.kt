e31d0b7eec846ad1ee391ea2694c4b19e4b24ddf
Ravil Galeyev <dehasi@yandex.ru>
2020-08-12 23:45:00 +0200

    Refactoring

--- src/main/kotlin/multithreaded/AuctionSearchConsumer.kt
+++ src/main/kotlin/multithreaded/AuctionSearchConsumer.kt
@@ -1,15 +1,7 @@
 package multithreaded
 
-class AuctionSearchConsumer(
-    private val executor: Executor,
-    private val houses: Collection<AuctionHouse>,
-    private val consumer: AuctionSearchConsumer) {
+interface AuctionSearchConsumer {
 
-    fun auctionSearchFound(findAuctions: Any) {
-        TODO("Not yet implemented")
-    }
-
-    fun auctionSearchFinished() {
-        TODO("Not yet implemented")
-    }
+    fun auctionSearchFound(auctions: List<AuctionDescription>)
+    fun auctionSearchFinished()
 }
