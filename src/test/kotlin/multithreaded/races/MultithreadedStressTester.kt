988c9a8f06d88e5e421fa192645d9d4d2b0c9099
Ravil Galeyev <dehasi@yandex.ru>
2020-08-13 23:47:26 +0200

    Add multithereaded exwcutor

--- src/test/kotlin/multithreaded/races/MultithreadedStressTester.kt
+++ src/test/kotlin/multithreaded/races/MultithreadedStressTester.kt
@@ -1,14 +1,40 @@
 package multithreaded.races
 
-class MultithreadedStressTester(val i: Int) {
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors.newCachedThreadPool
+
+class MultithreadedStressTester(
+    private val iterationCount: Int,
+    private val threadCount: Int = 2,
+    private val executor: ExecutorService = newCachedThreadPool()) {
+
+    fun totalActionCount() = threadCount * iterationCount
 
     fun stress(runnable: Runnable) {
-        TODO("Not yet implemented")
+        executor.execute(runnable)
     }
 
-    fun shutdown() {
-        TODO("Not yet implemented")
+    private fun spawnThreads(action: Runnable): CountDownLatch {
+        val finished = CountDownLatch(threadCount)
+
+        for (i in 0 until threadCount) {
+            executor.execute {
+                try {
+                    repeatAction(action)
+                } finally {
+                    finished.countDown()
+                }
+            }
+        }
+
+        return finished
+
     }
 
-    fun totalActionCount() = i
+    private fun repeatAction(action: Runnable) {
+        for (i in 0 until iterationCount) action.run()
+    }
+
+    fun shutdown() = executor.shutdown()
 }
