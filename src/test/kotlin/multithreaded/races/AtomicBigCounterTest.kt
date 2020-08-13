fee5a166a43f2b62f485c6e872ac2d7dad29f5ff
Ravil Galeyev <dehasi@yandex.ru>
2020-08-13 23:36:26 +0200

    Add AtomicBigCounterTest template

--- src/test/kotlin/multithreaded/races/AtomicBigCounterTest.kt
+++ src/test/kotlin/multithreaded/races/AtomicBigCounterTest.kt
@@ -1,5 +1,6 @@
 package multithreaded.races
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class AtomicBigCounterTest {
@@ -7,6 +8,13 @@ class AtomicBigCounterTest {
     val counter = AtomicBigCounter()
 
     @Test fun `can increment counter from multiple threads simultaneously`() {
+        val stressTester = MultithreadedStressTester(50_000)
 
+        stressTester.stress(Runnable {
+            counter.inc()
+        })
+        stressTester.shutdown()
+
+        assertThat(counter.count()).`as`("final count").isEqualTo(stressTester.totalActionCount())
     }
 }
---
+++ src/test/kotlin/multithreaded/races/MultithreadedStressTester.kt
@@ -0,0 +1,14 @@
+package multithreaded.races
+
+class MultithreadedStressTester(val i: Int) {
+
+    fun stress(runnable: Runnable) {
+        TODO("Not yet implemented")
+    }
+
+    fun shutdown() {
+        TODO("Not yet implemented")
+    }
+
+    fun totalActionCount() = i
+}
