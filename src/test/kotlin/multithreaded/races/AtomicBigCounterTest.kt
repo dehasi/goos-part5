b4a97bde7fe6fb917c29e389b973b596e54776b3
Ravil Galeyev <dehasi@yandex.ru>
2020-08-13 23:52:21 +0200

    Disable intentionally failing test

--- src/test/kotlin/multithreaded/races/AtomicBigCounterTest.kt
+++ src/test/kotlin/multithreaded/races/AtomicBigCounterTest.kt
@@ -1,12 +1,14 @@
 package multithreaded.races
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class AtomicBigCounterTest {
 
     private val counter = AtomicBigCounter()
 
+    @Disabled("to make build pass. The test should fail")
     @Test fun `can increment counter from multiple threads simultaneously`() {
         val stressTester = MultithreadedStressTester(50_000)
 
