d0c14c867a988183b7793810ec57b47489f62e74
Ravil Galeyev <dehasi@yandex.ru>
2020-08-13 23:31:51 +0200

    Add AtomicBigCounterTest.kt

---
+++ src/test/kotlin/multithreaded/races/AtomicBigCounterTest.kt
@@ -0,0 +1,12 @@
+package multithreaded.races
+
+import org.junit.jupiter.api.Test
+
+class AtomicBigCounterTest {
+
+    val counter = AtomicBigCounter()
+
+    @Test fun `can increment counter from multiple threads simultaneously`() {
+
+    }
+}
