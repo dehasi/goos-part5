7af6fe638f8e94d555e0a63d6453fa3c3d278a26
Ravil Galeyev <dehasi@yandex.ru>
2020-08-15 23:55:40 +0200

    Add maybe unknown

--- src/main/kotlin/maybe/Maybe.kt
+++ src/main/kotlin/maybe/Maybe.kt
@@ -9,6 +9,23 @@ abstract class Maybe<T> : Iterable<T> {
     abstract fun otherwise(maybeDefaultValue: Maybe<T>): Maybe<T>
     abstract fun <U> to(mapping: Function<in T, out U>): Maybe<U>
     abstract fun query(mapping: Predicate<in T>): Maybe<Boolean>
+
+    companion object {
+        fun <T> unknown(): Maybe<T> {
+            return object : Maybe<T>() {
+                override fun isKnown() = false
+
+                override fun otherwise(defaultValue: T) = defaultValue
+                override fun otherwise(maybeDefaultValue: Maybe<T>) = maybeDefaultValue
+
+                override fun <U> to(mapping: Function<in T, out U>): Maybe<U> = unknown()
+
+                override fun query(mapping: Predicate<in T>): Maybe<Boolean> = unknown()
+
+                override fun iterator() = listOf<T>().iterator()
+            }
+        }
+    }
 }
 
 
