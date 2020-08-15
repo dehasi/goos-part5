3c23d6311ce3ff71394fc252dd714be74fef44d6
Ravil Galeyev <dehasi@yandex.ru>
2020-08-15 23:48:50 +0200

    Add maybe

--- src/main/kotlin/maybe/Maybe.kt
+++ src/main/kotlin/maybe/Maybe.kt
@@ -1,4 +1,14 @@
 package maybe
 
-class Maybe <T> {
+import java.util.function.Function
+import java.util.function.Predicate
+
+abstract class Maybe<T> : Iterable<T> {
+    abstract fun isKnown(): Boolean
+    abstract fun otherwise(defaultValue: T): T
+    abstract fun otherwise(maybeDefaultValue: Maybe<T>): Maybe<T>
+    abstract fun <U> to(mapping: Function<in T, out U>): Maybe<U>
+    abstract fun query(mapping: Predicate<in T>): Maybe<Boolean>
 }
+
+
